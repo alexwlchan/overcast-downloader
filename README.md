3ee5ef554d7fb9150cbcf46bbece85e7df0d8ba1
Alex Chan <alex@alexwlchan.net>
2019-11-22 21:07:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# overcast-downloader
+
+This is a script that lets you download audio files for every podcast you've listened to in Overcast.
+
+I listen to a lot of podcasts, and I use [Overcast].
+Sometimes I want to listen to a podcast I heard a long time ago, but links rot, websites break, and sometimes
+
+[Overcast]: https://overcast.fm/
+
+![An old man sitting on a chair outdoors, listening to a record player.](record_player.jpg)
---
+++ download_overcast_podcasts.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+"""
+Download podcast files based on your Overcast export.
+
+If you have an Overcast account, you can download an OPML file with
+a list of every episode you've played from https://overcast.fm/account
+
+This tool can read that OPML file, and save a local copy of the audio files
+for every episode you've listened to.
+"""
+
+import argparse
+import errno
+import json
+import os
+import sys
+import xml.etree.ElementTree as ET
+
+try:
+    from urllib.error import HTTPError
+    from urllib.parse import urlparse
+    from urllib.request import urlretrieve
+except ImportError:  # Python 2.7
+    from urllib import urlretrieve
+    from urllib2 import HTTPError
+    from urlparse import urlparse
+
+
+def parse_args(argv):
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        "OPML_PATH",
+        help="Path to an OPML file downloaded from https://overcast.fm/account",
+    )
+
+    parser.add_argument(
+        "--out_dir", default="audiofiles", help="Directory to save audio files to"
+    )
+
+    args = parser.parse_args(argv)
+
+    return {
+        "opml_path": os.path.abspath(args.OPML_PATH),
+        "out_dir": os.path.abspath(args.out_dir),
+    }
+
+
+def get_episodes(xml_string):
+    """
+    Given the XML string of the Overcast OPML, generate a sequence of entries
+    that represent a single, played podcast episode.
+    """
+    root = ET.fromstring(xml_string)
+
+    # The Overcast OPML has the following form:
+    #
+    #   <?xml version="1.0" encoding="utf-8"?>
+    #   <opml version="1.0">
+    #       <head><title>Overcast Podcast Subscriptions</title></head>
+    #       <body>
+    #           <outline text="playlists">...</outline>
+    #           <outline text="feeds">...</outline>
+    #       </body>
+    #   </opml>
+    #
+    # Within the <outline text="feeds"> block of XML, there's a list of feeds
+    # with the following structure (some attributes omitted):
+    #
+    #   <outline type="rss"
+    #            title="My Example Podcast"
+    #            xmlUrl="https://example.org/podcast.xml">
+    #       <outline type="podcast-episode"
+    #                overcastId="12345"
+    #                pubDate="2001-01-01T01:01:01-00:00"
+    #                title="The first episode"
+    #                url="https://example.net/podcast/1"
+    #                overcastUrl="https://overcast.fm/+ABCDE"
+    #                enclosureUrl="https://example.net/files/1.mp3"/>
+    #       ...
+    #   </outline>
+    #
+    # We use an XPath expression to find the <outline type="rss"> entries
+    # (so we get the podcast metadata), and then find the individual
+    # "podcast-episode" entries in that feed.
+
+    for feed in root.findall("./body/outline[@text='feeds']/outline[@type='rss']"):
+        podcast = {
+            "title": feed.get("title"),
+            "text": feed.get("text"),
+            "xml_url": feed.get("xmlUrl"),
+        }
+
+        for episode_xml in feed.findall("./outline[@type='podcast-episode']"):
+            episode = {
+                "published_date": episode_xml.get("pubDate"),
+                "title": episode_xml.get("title"),
+                "url": episode_xml.get("url"),
+                "overcast_id": episode_xml.get("overcastId"),
+                "overcast_url": episode_xml.get("overcastUrl"),
+                "enclosure_url": episode_xml.get("enclosureUrl"),
+            }
+
+            yield {
+                "podcast": podcast,
+                "episode": episode,
+            }
+
+
+def mkdir_p(path):
+    """Create a directory if it doesn't already exist."""
+    try:
+        os.makedirs(path)
+    except OSError as err:
+        if err.errno == errno.EEXIST:
+            pass
+        else:
+            raise
+
+
+def download_episode(episode, out_dir):
+    """
+    Given a blob of episode data from get_episodes, download the MP3 file and
+    save the metadata to ``out_dir``.
+    """
+    # If the MP3 URL is https://example.net/mypodcast/podcast1.mp3,
+    # get the filename ``podcast1.mp3``.
+    audio_url = episode["episode"]["enclosure_url"]
+    url_path = urlparse(audio_url).path
+    filename = os.path.basename(url_path)
+
+    # Within the out_dir, put the episodes for each podcast in the
+    # same folder.
+    podcast_dir = os.path.join(out_dir, episode["podcast"]["title"].replace("/", "_"))
+    mkdir_p(podcast_dir)
+
+    # Download the podcast audio file if it hasn't already been downloaded.
+    download_path = os.path.join(podcast_dir, filename)
+
+    if os.path.exists(download_path):
+        print("*** Already downloaded %s, skipping" % audio_url)
+    else:
+        print("*** Downloading %s" % audio_url)
+        try:
+            tmp_path, _ = urlretrieve(audio_url)
+        except HTTPError as err:
+            print("*** Error downloading audio file: %s" % err)
+        else:
+            print("*** Download successful!")
+            os.rename(tmp_path, download_path)
+            filename = None
+
+    # Save a blob of JSON with some episode metadata
+    if filename is not None:
+        episode["filename"] = filename
+
+    json_string = json.dumps(episode, indent=2, sort_keys=True)
+    json_path = download_path + ".json"
+
+    with open(json_path, "w") as outfile:
+        outfile.write(json_string)
+
+
+if __name__ == "__main__":
+    args = parse_args(argv=sys.argv[1:])
+
+    opml_path = args["opml_path"]
+    out_dir = args["out_dir"]
+
+    try:
+        with open(opml_path) as infile:
+            xml_string = infile.read()
+    except OSError as err:
+        if err.errno == errno.ENOENT:
+            sys.exit("Could not find an OPML file at %s" % opml_path)
+        else:
+            raise
+
+    for episode in get_episodes(xml_string):
+        download_episode(episode=episode, out_dir=out_dir)
